588189f3bfc67672ff3f7b451fc1da9536eabaf3
Manuel Perez <Manuel.Perez2@thomsonreuters.com>
2023-08-13 09:04:10 -0400

    Bodega CRUD

---
+++ SistemaInventarioV7/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SistemaInventarioV7.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV7.Modelos;
+using SistemaInventarioV7.Utilidades;
+
+namespace SistemaInventarioV7.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Bodega bodega = new Bodega();
+
+            if (id == null)
+            {
+                //Crear nueva bodega
+                bodega.Estado = true;
+                return View(bodega);
+            }
+            //Actualizamos bodega
+            bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+            if (bodega == null)
+            {
+                return NotFound();
+            }
+            return View(bodega);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Bodega bodega)
+        {
+            if (ModelState.IsValid)
+            {
+                if (bodega.Id == 0)
+                {
+                    await _unidadTrabajo.Bodega.Agregar(bodega);
+                    TempData[DS.Exitosa] = "Bodega creada exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Bodega.Actualizar(bodega);
+                    TempData[DS.Exitosa] = "Bodega actualizada exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al intentar guardar";
+            return View(bodega);
+        }
+
+        #region API
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Bodega.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Bodega bodega = await _unidadTrabajo.Bodega.Obtener(id);
+            if (bodega == null) { return Json(new { success = false, message = "Error al eliminar bodega" }); }
+            _unidadTrabajo.Bodega.Eliminar(bodega);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "La bodega se ha eliminado correctamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var listadoNombres = await _unidadTrabajo.Bodega.ObtenerTodos();
+            valor = id == 0 ? listadoNombres.Any(b => b.Nombre == nombre): listadoNombres.Any(b => b.Nombre == nombre && b.Id != id);
+            return Json(new { data = valor });
+        }
+
+        #endregion
+    }
+}
